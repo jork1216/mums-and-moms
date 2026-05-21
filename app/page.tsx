50dce6e13622bc3ef4c655b415e6ea0a14644920
junkcarlo16 <ramosjohncarlo16@gmail.com>
2026-05-21 23:19:09 +0800

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
             alt=""
             fill
             sizes="(max-width: 500px) 100vw, 500px"
-            className="translate-x-[5%] translate-y-6 scale-[1.1] object-contain object-right-bottom"
+            className="translate-x-[5%] translate-y-2 scale-[1.1] object-contain object-right-bottom"
           />
         </div>
 
